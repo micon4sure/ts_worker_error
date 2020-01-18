60336f7c709212ed36aab101d411a839e4fc0c42
mICON <micon@micon.tech>
2020-01-18 11:57:26 +0100

    initial

---
+++ start.ts
@@ -0,0 +1,4 @@
+
+import { Worker } from 'worker_threads'
+
+const worker = new Worker("./workerAdapter.js", { workerData: { file: __dirname + '/worker.ts' } });
---
+++ worker.ts
@@ -0,0 +1,2 @@
+console.log('worker spawned')
+asd();
